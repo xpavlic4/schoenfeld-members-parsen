883e9deeb046560b0c5260c542da46fc213e11fd
Radim Pavlicek <radim.pavlicek@gmail.com>
2017-09-12 14:35:14 +0200

    organize imports

--- src/main/scala/ParserApp.scala
+++ src/main/scala/ParserApp.scala
@@ -1,14 +1,5 @@
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
-
-import com.typesafe.config.ConfigFactory
-import net.ruippeixotog.scalascraper.browser.JsoupBrowser
-import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL._
-import net.ruippeixotog.scalascraper.model.Element
-import net.ruippeixotog.scalascraper.util.ProxyUtils
-import net.ruippeixotog.scalascraper.util.Validated._
-
-import scala.collection.immutable.SortedMap
 /**
   * Created by pavlic1 on 24/01/2017.
   */
